c34cf0235bfb653de3e9a89155c7628ec532e82a
Tubsmcgee <admhartman@gmail.com>
2017-10-09 21:07:20 -0500

    Derp
    changed excel sheet and App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,25 @@ import xlsx from 'xlsx';
 import './App.css';
 
 class App extends Component {
-  changeFile(e){
-    const file = e.target.files[0];
+  constructor(props){
+    super(props);
+    this.state = {rows: []};
+  }
+  changeFile(file){
+    if(!file) return;
     const reader = new FileReader();
     reader.onload = (e)=>{
       const data = btoa(e.target.result);
-      const parsed = xlsx.read(data);
+      const parsed = xlsx.read(data).Sheets.Sheet1;
+      const rows = [];
+      for (let row = 0; row < 26; row++){
+        rows[row]=[];
+        for (let col = 0; col < 26; col++){
+          const key = String.fromCharCode(col + 65) + (row + 1);
+          if(parsed[key]) rows[row][col] = parsed[key];
+        }
+      }
+      this.setState({rows});
       console.log(parsed);
     }
     reader.readAsBinaryString(file);
@@ -20,11 +33,20 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React, don't touch my penis</h1>
-          <input type="file" onChange={this.changeFile}/>
+          <input type="file" onChange={e => this.changeFile(e.target.files[0])}/>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <table>
+          <tbody>
+            {this.state.rows.map((row, i) =>
+              <tr key={i}>{row.map((col, j) =>
+                <td key={j} title={JSON.stringify(col)}>
+                  {col ? col.v : ''}
+                </td>
+              )}
+              </tr>
+            )}
+          </tbody>
+        </table>
       </div>
     );
   }
