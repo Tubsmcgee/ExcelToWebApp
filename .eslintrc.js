2b608c065bfcc5d3e69dd7001fe4ce941849ca9a
Andrew Maxwell <andrew.maxwell@monsanto.com>
2017-10-09 21:28:24 -0500

    Added linting stuff

---
+++ .eslintrc.js
@@ -0,0 +1,69 @@
+module.exports = {
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    mocha: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'plugin:import/errors',
+    'plugin:import/warnings'
+  ],
+  plugins: ['react', 'prettier'],
+  settings: {
+    'import/resolver': 'webpack'
+  },
+  rules: {
+    'prefer-const': 'warn',
+    'prefer-arrow-callback': 'warn',
+    'no-var': 'error',
+    'no-bitwise': 'error',
+    'jsx-quotes': ['warn', 'prefer-double'],
+    'func-style': 'warn',
+    'no-octal-escape': 'error',
+    'no-implicit-globals': 'error',
+    'no-fallthrough': 'off',
+    'no-extend-native': 'error',
+    'no-empty-function': 'warn',
+    'no-caller': 'error',
+    'no-alert': 'warn',
+    'no-use-before-define': [
+      'warn',
+      {
+        functions: true,
+        classes: true
+      }
+    ],
+    eqeqeq: 'warn',
+    'block-scoped-var': 'error',
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'require-yield': 'off',
+    'no-duplicate-imports': 'error',
+    'dot-notation': 'warn',
+    'quote-props': ['warn', 'as-needed'],
+    'arrow-body-style': ['warn', 'as-needed'],
+    'object-shorthand': 'warn',
+    'prettier/prettier': [
+      'warn',
+      {
+        printWidth: 120,
+        singleQuote: true,
+        bracketSpacing: false
+      }
+    ]
+  }
+};
